1fcd6cdde8cfc85f71599ba9376a7c1265c79386
CEKTOP77 <kokodgambo33@gmail.com>
2025-11-14 02:08:36 +0300

    Update universal_mrz_app.py

--- universal_mrz_app.py
+++ universal_mrz_app.py
@@ -41,10 +41,10 @@ def generate_mrz_td3(doc_type, country, nationality,
     final_cd = mrz_check_digit(body)
 
     # теперь гарантируем длину 44 и ставим финальный чек‑символ последним
-    if len(body) >= 43:
+    if len(body) >= 42:
         line2 = body[:42] + final_cd
     else:
-        line2 = body.ljust(43, "<") + final_cd
+        line2 = body.ljust(42, "<") + final_cd
 
     return [line1, line2]
 
